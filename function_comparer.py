394d7ebc938d2b1bb284178bee38e1dbbc7c8a01
Zeleczek-kodowniczek <jakub.zelek.111@gmail.com>
2023-12-04 14:31:37 +0100

    test2

--- function_comparer.py
+++ function_comparer.py
@@ -1,31 +1,24 @@
-    def wraper_predict(self, x, num_window):
-        assert len(x.shape) == 2
+    def get_model_inputs(self, x, window_len):
 
         dem_x, ts_x = x[:, :self.num_dem_ftr].copy(), x[:, self.num_dem_ftr:].copy()
-        
+
         # initializing the value of all arrays
-        ts_x_ = np.zeros((x.shape[0], self.num_ts_step, self.num_ts_ftr))
+        ts_x_ = np.zeros((x.shape[0], self.all_ts.shape[1], self.all_ts.shape[2]))
         mask_x_ = np.zeros_like(ts_x_)
         dem_x_ = np.zeros_like(dem_x, dtype=float)
-        tstep = np.ones((x.shape[0], self.num_ts_step, 1)) * \
-                np.reshape(np.arange(0, self.num_ts_step), (1, self.num_ts_step, 1))
-        
-        # Reshaping the ts indices based on the time windows for each feature
-        ## Specific to Binary Time Window
-        temp_ts_x = np.zeros((ts_x.shape[0], max(num_window), self.num_ts_ftr), dtype=int)
-        for i in range(self.num_ts_ftr):
-            temp_ts_x[:, :num_window[i], i] = ts_x[:, sum(num_window[:i]):sum(
-                num_window[:i + 1])]
-        ts_x = temp_ts_x
+        tstep = np.ones((x.shape[0], self.all_ts.shape[1], 1)) * \
+                np.reshape(np.arange(0, self.all_ts.shape[1]), (1, self.all_ts.shape[1], 1))
+
+        # Reshaping the ts indices based on the num time windows and features
+        ts_x = ts_x.reshape((ts_x.shape[0], self.num_windows, self.num_ts_ftr))
 
         for i in range(x.shape[0]):
             # creating time series data
-            for j in range(self.num_ts_ftr):
-                # Finding the corresponding time interval
-                wind_t = np.searchsorted(self.split_points[j], np.arange(
-                    self.num_ts_step))  ## Specific to Binary Time Window
-                for t in range(self.num_ts_step):
-                    ind = ts_x[i, wind_t[t], j]
+            for t in range(self.num_ts_step):
+                for j in range(self.num_ts_ftr):
+                    # Finding the corresponding time interval
+                    wind_t = np.ceil((t + 1) / window_len).astype('int') - 1
+                    ind = ts_x[i, wind_t, j]
                     ts_x_[i, t, j] = self.all_ts[ind, t, j]
                     mask_x_[i, t, j] = None if self.all_mask is None else self.all_mask[ind, t, j]
             # creating static data
@@ -35,4 +28,5 @@
 
         # Creating the input of the model based on the different models. 
         # This part should be updated as new models get involved in the project
+
         return ts_x_, dem_x_, tstep
